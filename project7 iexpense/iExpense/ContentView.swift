71a9c3ee0e9a304a66520608727f36d207c61c45
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2022-04-10 08:37:27 -0500

    Add files via upload

---
+++ project7 iexpense/iExpense/ContentView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            
+                            Text(item.name)
+                                .font(.headline)
+                            
+                            Text(item.type)
+                        }
+
+                        Spacer()
+
+                        if(item.amount>10.00){Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                            .foregroundColor(item.amount>100.00 ? .red : .orange)
+                        }
+                        else{Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD")).foregroundColor(.green)
+                            
+                        }
+                            
+                    }
+                }
+                .onDelete(perform: removeItems)
+                }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
