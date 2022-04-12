a31d53ef2e8237eb5ae6d7fd32216e0316092472
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2022-04-12 00:02:06 -0500

    Add files via upload

---
+++ project12 CoreData/Project12/Movie+CoreDataProperties.swift
@@ -0,0 +1,12 @@
+import Foundation
+import CoreData
+
+extension Movie {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String
+    @NSManaged public var director: String
+    @NSManaged public var year: Int16
+}
