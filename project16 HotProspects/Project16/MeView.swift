97c293a5baab0cf66aa58363a4202d140424860d
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2022-04-12 00:00:07 -0500

    Add files via upload

---
+++ project16 HotProspects/Project16/MeView.swift
@@ -0,0 +1,71 @@
+import CoreImage.CIFilterBuiltins
+import SwiftUI
+
+struct MeView: View {
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
+
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .contextMenu{
+                        Button{
+                            let image = generateQRCode(from: "\(name)\n\(emailAddress)")
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: image)
+                        } label: {
+                            Label("Save to Photos",systemImage: "square.and.arrow.down")
+                        }
+                    }
+            
+                Spacer()
+            }
+            .navigationBarTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
+        }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
+    }
+
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
